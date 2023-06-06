8da8aef0c11276506999b035ff7ed66c73c23067
Harald Pretl <harald.pretl@jku.at>
2023-06-06 17:54:49 +0200

    Fix layver.sh

--- layver.sh
+++ layver.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 RESDIR=$PWD/layver.tmp
-[ -d $RESDIR ] && rm -rf $RESDIR
-mkdir -p $RESDIR
+[ -d "$RESDIR" ] && rm -rf "$RESDIR"
+mkdir -p "$RESDIR"
 
 ERROR=0
 
 # Run DRC
-./iic-drc.sh -o $RESDIR -b power_gate.mag || ERROR=1
+./iic-drc.sh -o "$RESDIR" -b power_gate.mag || ERROR=1
 # Run LVS
-./iic-lvs.sh -o $RESDIR -s power_gate.sch -l power_gate.mag -c power_gate || ERROR=1
+./iic-lvs.sh -o "$RESDIR" -s power_gate.sch -l power_gate.mag -c power_gate || ERROR=1
 
-if [ ERROR = 1 ]; then
+if [ $ERROR = 1 ]; then
 	echo "==="
 	echo "[ERROR] Layout verification FAILED! Check results!"
 	exit 1

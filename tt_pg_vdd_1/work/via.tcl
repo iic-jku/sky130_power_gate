3823ba3bfa260d82956c3ca60525c0957515b324
Harald Pretl <harald.pretl@jku.at>
2023-10-19 16:13:16 +0200

    Cell `tt_pg_vdd_1` finished, DRC and LVS clean

---
+++ tt_pg_vdd_1/work/via.tcl
@@ -0,0 +1,11 @@
+box grow e 0.05um
+box grow w 0.05um
+paint m1
+box grow n -0.05um
+box grow s -0.05um
+paint v1,v2,v3
+box grow n 0.05um
+box grow s 0.05um
+box grow e 0.05um
+box grow w 0.05um
+paint m2,m3,m4
